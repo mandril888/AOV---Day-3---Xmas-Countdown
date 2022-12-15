80b85e689b21905aaebed2d7b3c1925fdb862736
mandril888 <arturo.rubio.89.8@gmail.com>
2022-12-15 18:16:15 +0100

    count down done

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@ const christmas = new Date('12/25/2022 00:00:00')
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" number="00" />
-          <CountdownSegment label="hours" number="00" />
-          <CountdownSegment label="minutes" number="00" />
-          <CountdownSegment label="seconds" number="00" />
+          <CountdownSegment label="days" :number="christmas.getDate() - 1 - now.getDate()" />
+          <CountdownSegment label="hours" :number="23 - now.getHours()" />
+          <CountdownSegment label="minutes" :number="59 - now.getMinutes()" />
+          <CountdownSegment label="seconds" :number="59 - now.getSeconds()" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
@@ -32,5 +32,15 @@ div {
 
 body {
   @apply bg-gray-100;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
